20a966190464165cac77ed2f7bba170fcd74bd74
Victor J. Rosario V <victorrosario.1890@gmail.com>
2023-01-02 08:48:26 -0400

    init project

---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+// import "mypackage"
+
+func main() {
+
+	// var car = mypackage.CarPublic{}
+	// car.Year = 200
+
+}
---
+++ src/mypackage/mypackage.go
@@ -0,0 +1,12 @@
+package mypackage
+
+// CarPublic this class / struct is public
+type CarPublic struct {
+	Brand string
+	Year  int
+}
+
+type carPrivate struct {
+	brand string
+	year  int
+}
